82d21c156e192af69103cad4b6e12ae5715d9da3
Revathy <revathy.paramasivam.9@gmail.com>
2022-02-10 12:03:24 +0530

    Partial Design Home Page

---
+++ assets/js/product_panel.js
@@ -0,0 +1,61 @@
+$(document).ready(function(){
+  $("#panel1Content").on('shown.bs.collapse',function(){
+    $(".panel1Icon").removeClass("fa-arrow-down").addClass("fa-arrow-up");
+  });
+  $("#panel1Content").on('hidden.bs.collapse',function(){
+    $(".panel1Icon").removeClass("fa-arrow-up").addClass("fa-arrow-down");
+  });
+  $("#panel2Content").on('shown.bs.collapse',function(){
+    $(".panel2Icon").removeClass("fa-arrow-down").addClass("fa-arrow-up");
+  });
+  $("#panel2Content").on('hidden.bs.collapse',function(){
+    $(".panel2Icon").removeClass("fa-arrow-up").addClass("fa-arrow-down");
+  });
+  $("#panel3Content").on('shown.bs.collapse',function(){
+    $(".panel3Icon").removeClass("fa-arrow-down").addClass("fa-arrow-up");
+  });
+  $("#panel3Content").on('hidden.bs.collapse',function(){
+    $(".panel3Icon").removeClass("fa-arrow-up").addClass("fa-arrow-down");
+  });
+
+  $("#panel4Content").on('shown.bs.collapse',function(){
+    $(".panel4Icon").removeClass("fa-arrow-down").addClass("fa-arrow-up");
+  });
+  $("#panel4Content").on('hidden.bs.collapse',function(){
+    $(".panel4Icon").removeClass("fa-arrow-up").addClass("fa-arrow-down");
+  });
+  $("#panel5Content").on('shown.bs.collapse',function(){
+    $(".panel5Icon").removeClass("fa-arrow-down").addClass("fa-arrow-up");
+  });
+  $("#panel5Content").on('hidden.bs.collapse',function(){
+    $(".panel5Icon").removeClass("fa-arrow-up").addClass("fa-arrow-down");
+  });
+  $("#panel6Content").on('shown.bs.collapse',function(){
+    $(".panel6Icon").removeClass("fa-arrow-down").addClass("fa-arrow-up");
+  });
+  $("#panel6Content").on('hidden.bs.collapse',function(){
+    $(".panel6Icon").removeClass("fa-arrow-up").addClass("fa-arrow-down");
+  });
+  
+  $(".check").on('click',function(e){
+    // console.log('clicked');
+    $("#panel1Content").collapse('show');
+    $("#panel2Content").collapse('show');
+    $("#panel3Content").collapse('show');
+    $("#panel4Content").collapse('show');
+    $("#panel5Content").collapse('show');
+    $("#panel6Content").collapse('show');
+  });
+  
+  $(".doCheck").on('click',function(e){
+    // $("#panel1Content").collapse({toggle:true});
+    // $("#panel2Content").collapse({toggle:false});
+    // $("#panel3Content").collapse({toggle:false});
+    $("#panel1Content").collapse('hide');
+    $("#panel2Content").collapse('hide');
+    $("#panel3Content").collapse('hide');
+    $("#panel4Content").collapse('hide');
+    $("#panel5Content").collapse('hide');
+    $("#panel6Content").collapse('hide');
+  });
+});
